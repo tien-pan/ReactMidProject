ba9edc0562436c8f763ba0e291c11125716ee115
tien-pan <tien0601@gms.tku.edu.tw>
2020-10-20 22:44:30 +0800

    Updates

---
+++ precache-manifest.dc1bb03f4cbfb0e07527d5dc31e6f6ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd3a2f601155d46fcb62dc82aa2bfd",
+    "url": "/ReactMidProject/index.html"
+  },
+  {
+    "revision": "a9accde43e761ae1914b",
+    "url": "/ReactMidProject/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "73aab52337eb4ecc732c",
+    "url": "/ReactMidProject/static/css/main.780d60e0.chunk.css"
+  },
+  {
+    "revision": "a9accde43e761ae1914b",
+    "url": "/ReactMidProject/static/js/2.ac1baccf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactMidProject/static/js/2.ac1baccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73aab52337eb4ecc732c",
+    "url": "/ReactMidProject/static/js/main.566fbde5.chunk.js"
+  },
+  {
+    "revision": "6786498d08575146b4e0",
+    "url": "/ReactMidProject/static/js/runtime-main.db966e0b.js"
+  },
+  {
+    "revision": "6b6170fe52fb23f505b4e056fefd2679",
+    "url": "/ReactMidProject/static/media/Pacifico.6b6170fe.ttf"
+  }
+]);
